918122b91142ebe7ee4322cbfc94155a61070d74
Tianlong Jia <2533433765@qq.com>
2025-10-25 23:36:46 +0200

    upload task for 1582 basins

---
+++ notebooks/LSTM_1582_basins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name="forecast_1582_basins"
+#SBATCH --ntasks=1
+#SBATCH --time=72:00:00
+#SBATCH --partition=normal
+#SBATCH --mem=501600
+#SBATCH --gres=gpu:full:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=tianlong.jia@kit.edu
+
+source /hkfs/home/haicore/iwu/qa8171/env/HY3.11/bin/activate
+
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/notebooks/LSTM_Forecast_DE.py
+
