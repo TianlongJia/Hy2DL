7bc12c32a2c74b4d4ebfc1925faa70f80cd5b1e7
Tianlong Jia <2533433765@qq.com>
2025-12-01 21:39:31 +0100

    upload baseline

---
+++ CPC_notebooks/CPC_baseline_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name="CPC_baseline"
+#SBATCH --ntasks=1
+#SBATCH --time=72:00:00
+#SBATCH --partition=normal
+#SBATCH --mem=501600
+#SBATCH --gres=gpu:full:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=tianlong.jia@kit.edu
+
+unset LD_LIBRARY_PATH  # load cuDNN intalled along with pytorch, rather than the cuDNN in Haicore system
+
+source /hkfs/home/haicore/iwu/qa8171/env/HY3.11/bin/activate
+
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_Alex_baseline_1582_basin.py
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_100per_emb.py
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_80per_emb.py
---
+++ CPC_notebooks/CPC_baseline_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="CPC_baseline"
+#SBATCH --ntasks=1
+#SBATCH --time=72:00:00
+#SBATCH --partition=normal
+#SBATCH --mem=501600
+#SBATCH --gres=gpu:full:1
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=tianlong.jia@kit.edu
+
+unset LD_LIBRARY_PATH  # load cuDNN intalled along with pytorch, rather than the cuDNN in Haicore system
+
+source /hkfs/home/haicore/iwu/qa8171/env/HY3.11/bin/activate
+
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_60per_emb.py
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_40per_emb.py
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_20per_emb.py
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_10per_emb.py
+python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_5per_emb.py
