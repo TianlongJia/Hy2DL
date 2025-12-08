3ffc5172b9728c5a09f44f9b8370639fddb67d3e
Tianlong Jia <2533433765@qq.com>
2025-12-08 15:52:18 +0100

    update

--- CPC_notebooks/CPC_baseline.sh
+++ CPC_notebooks/CPC_baseline.sh
@@ -12,10 +12,17 @@ unset LD_LIBRARY_PATH  # load cuDNN intalled along with pytorch, rather than the
 
 source /hkfs/home/haicore/iwu/qa8171/env/HY3.11/bin/activate
 
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_100per_NoEmb.py
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_80per_NoEmb.py
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_60per_NoEmb.py
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_40per_NoEmb.py
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_20per_NoEmb.py
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_10per_NoEmb.py
-python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_5per_NoEmb.py
+Percent=1 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+Percent=0.8 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+Percent=0.6 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+Percent=0.4 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+Percent=0.2 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+Percent=0.1 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+Percent=0.05 python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_NoEmb.py
+
+# python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_80per_NoEmb.py
+# python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_60per_NoEmb.py
+# python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_40per_NoEmb.py
+# python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_20per_NoEmb.py
+# python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_10per_NoEmb.py
+# python /hkfs/home/haicore/iwu/qa8171/Project/Hy2DL/CPC_notebooks/CPC_baseline_5per_NoEmb.py
